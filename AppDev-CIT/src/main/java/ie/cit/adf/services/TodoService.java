1e9942dc58c8000095b3a46e8dbc4cb85787b46d
T410 <T410@DH-LNVO-LAPTOP>
2017-12-10 19:06:04 +0000

    First Commit

---
+++ AppDev-CIT/src/main/java/ie/cit/adf/services/TodoService.java
@@ -0,0 +1,21 @@
+package ie.cit.adf.services;
+
+import ie.cit.adf.domain.Todo;
+
+import java.util.List;
+
+public interface TodoService {
+
+	List<Todo> getAllTodos();
+
+	Todo createNewTodo(String text);
+
+	void close(String todoId);
+
+	void open(String todoId);
+
+	Todo get(String todoId);
+
+	void delete(String todoId);
+
+}
